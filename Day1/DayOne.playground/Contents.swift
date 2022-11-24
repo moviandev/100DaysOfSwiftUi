cbb0aa2dadbba203d113eea63c45e96023df83cd
Matheus Viana <matheus_o_viana@hotmail.com>
2022-11-23 23:35:52 -0300

    Day 1 of 100daysOfSwiftUi third lesson learnt how to create integers and how to use number separation to make reading easier and helper methods

--- Day1/DayOne.playground/Contents.swift
+++ Day1/DayOne.playground/Contents.swift
@@ -27,3 +27,25 @@ print(quote.uppercased())
 
 print(quote.hasPrefix(" A day in"))
 print(quote.hasSuffix("engineer"))
+
+let score = 10
+let reallyBig = 100_000_000
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubleScore = score * 2
+let squareScore = score * score
+let halvedScore = score / 2
+
+var counter = 10
+counter += 5
+print(counter)
+
+counter *= 2
+counter -= 10
+counter /= 2
+
+
+let number = 120
+print(number.isMultiple(of: 3))
+print(120.isMultiple(of: 3))
