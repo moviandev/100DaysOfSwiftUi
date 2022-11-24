b8b1c79913e4520602858c3cb583a1311eda6c4d
Matheus Viana <matheus_o_viana@hotmail.com>
2022-11-23 23:07:20 -0300

    Day 1 of 100daysOfSwiftUi first lesson learnt about how declare variables and the diference between variable and constants

---
+++ Day1/DayOne.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+var name = "Ross"
+print(name)
+
+name = "Rachel"
+print(name)
+
+name = "Joe"
+print(name)
+
+let character = "Monica"
+
+let managerName = "Michael Scott"
+let dogBreed = "Shitzu"
+
