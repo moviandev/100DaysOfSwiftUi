03a300704a25f68f25e5849c6388769f4afd55a0
Matheus Viana <matheus_o_viana@hotmail.com>
2022-11-24 22:45:05 -0300

    Day 2 of 100daysOfSwiftUi second lesson learnt how to do string interpolation between strings and adding other types of variables

--- Day2/DayTwo.playground/Contents.swift
+++ Day2/DayTwo.playground/Contents.swift
@@ -13,3 +13,16 @@ isAuthenticated = !isAuthenticated
 print(isAuthenticated)
 isAuthenticated = !isAuthenticated
 print(isAuthenticated)
+
+let firstPart = "Hello, "
+let secondPart = "World!"
+let greetings = firstPart + secondPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old"
+
