be994414d15d4c2ee658b8a8bcac3031c7048960
Matheus Viana <matheus_o_viana@hotmail.com>
2022-11-27 20:17:19 -0300

    Day 5 of 100Days of SwiftUI second lesson, learnt how to check multi-conditions

--- Day5/Day5.playground/Contents.swift
+++ Day5/Day5.playground/Contents.swift
@@ -62,3 +62,41 @@ if username.isEmpty {
 
 print("Welcome, \(username)")
 
+let age1 = 16
+
+if age1 >= 18 {
+    print("You can vote in the next election")
+} else {
+    print("Sorry, You're too young to vote")
+}
+
+let temp = 25
+
+// bad code
+if temp > 20 && temp < 30 {
+    print("It's a nice day")
+}
+
+let userAge = 14
+let hasParentalConsent =  true
+
+if age >= 18 || hasParentalConsent {
+    print("You can buy the game!")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, escooter
+}
+
+let transport = TransportOption.bicycle
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if (transport == .bicycle) {
+    print("I hpoe there's a bike path...")
+} else if (transport == .car) {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a escooter now!")
+}
+
