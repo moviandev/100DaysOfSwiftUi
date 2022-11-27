5681de9f8be93018fe9fbe824ecdaaf589ca1130
Matheus Viana <matheus_o_viana@hotmail.com>
2022-11-26 21:53:08 -0300

    Day 4 of 100Days of SwiftUI first lesson learnt about how to use type annotations  and the importance of it

---
+++ Day4/Day4.playground/Contents.swift
@@ -0,0 +1,40 @@
+import Cocoa
+
+let surname: String = "Lasso"
+var score: Double = 0
+score = 1.4
+score = 4
+
+let playerName: String = "Roy"
+let luckyNumber: Int = 13
+let pi: Double = 3.141
+let isAuthenticate: Bool = true
+
+var albums: [String] = ["Red", "Fearless"]
+var user: [String: String] = [
+    "Id": "@twostraws"
+]
+var books: Set<String> = Set([
+    "The Bluest eye",
+    "Foundation",
+    "Girl, Woman, Other"
+])
+
+var soda: [String] = ["Coke", "Pepsi", "Irn-Bru"]
+var teams: [String] = [String]()
+var cities: [String] = []
+var clues = [String]()
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style: UIStyle = .light
+style = .dark
+
+let username: String
+// lots of complex logic
+username = "@twostraws"
+// lots of complex logic
+print(username)
+
