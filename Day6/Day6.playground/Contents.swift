b17da54b7feea3ac619caedcedff6616c5bf2bfa
Matheus Viana <matheus_o_viana@hotmail.com>
2022-11-28 22:42:45 -0300

    Day 5 of 100Days of SwiftUI second lesson, learnt how to use while loops  and some useful methods

--- Day6/Day6.playground/Contents.swift
+++ Day6/Day6.playground/Contents.swift
@@ -36,3 +36,24 @@ for _ in 1...5 {
 }
 
 print(lyric)
+
+var countdown = 10
+
+while countdown > 0 {
+    print("\(countdown)...")
+    countdown -= 1
+}
+
+print("Blast off!")
+
+let id = Int.random(in: 1...1000)
+let amount = Double.random(in: 0...1)
+
+var roll = 0
+
+while roll != 20 {
+    roll = Int.random(in: 1...20)
+    print("I rolled a \(roll)")
+}
+
+print("Critical hit!")
