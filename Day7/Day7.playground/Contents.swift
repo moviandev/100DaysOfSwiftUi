e61cc6147807c09e316462fddc90e2e14895df9f
Matheus Viana <matheus_o_viana@hotmail.com>
2022-11-29 22:41:40 -0300

    Day 7 of 100Days of SwiftUI, fourth lesson: learnt how to customize parameter names

--- Day7/Day7.playground/Contents.swift
+++ Day7/Day7.playground/Contents.swift
@@ -65,6 +65,45 @@ func getUser() -> (firstName: String, lastName: String) {
 let (firstName, lastName) = getUser()
 print("First name: \(firstName) surname: \(lastName)")
 
+func rollDice2(sides: Int, count: Int) -> [Int] {
+    var rolls = [Int]()
+    
+    for _ in 1...count {
+        let roll = Int.random(in: 1...sides)
+        rolls.append(roll)
+    }
+    
+    return rolls
+}
 
+let rolls = rollDice2(sides: 6, count: 4)
 
+func hireEmployee(name: String) {
+    
+}
 
+func hireEmployee(title: String) {
+    
+}
+
+func hireEmployee(location: String) {
+    
+}
+
+let lyric = "I see a red door and I want it painted black"
+print(lyric.hasPrefix("I see"))
+
+func isUppercase(_ string: String) -> Bool {
+    string == string.uppercased()
+}
+
+let str = "HELLO WORLD!"
+let result1 = isUppercase(str)
+
+func printTimesTable(for number: Int) {
+    for i in 1...12 {
+        print("\(i) * \(number) is \(i * number)")
+    }
+}
+
+printTimesTable(for: 5)
