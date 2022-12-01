eb2b5acc9e2843d8a3017ca8e42658a201689291
Matheus Viana <matheus_o_viana@hotmail.com>
2022-11-30 22:45:19 -0300

    Day 8 of 100Days of SwiftUI, second lesson: learnt how to handle errors in functions

--- Day8/Day8.playground/Contents.swift
+++ Day8/Day8.playground/Contents.swift
@@ -14,3 +14,43 @@ var characters = ["Ross", "Rachel", "Monica"]
 print(characters.count)
 characters.removeAll(keepingCapacity: true)
 print(characters.count)
+
+
+enum PasswordError: Error {
+    case short, obvious
+}
+
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+    
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+    
+    if password.count < 8 {
+        return "Ok"
+    } else if password.count < 10 {
+        return "Good"
+    } else {
+        return "Excellent"
+    }
+}
+
+let string = "1234"
+
+do {
+    let result = try checkPassword(string)
+    print("Password rating: \(result)")
+} catch PasswordError.short {
+    print("Please use a longer password!")
+} catch PasswordError.obvious {
+    print("I have the same combination on my luggage!")
+} catch {
+    print("There was an error: \(error.localizedDescription)")
+}
+
+//let result1 = try! checkPassword(string)
+//print("Password rating: \(result1)")
+
