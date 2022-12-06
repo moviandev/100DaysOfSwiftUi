0a9a71bf28a0c0debbd4d93c1045fc1610a209f5
Matheus Viana <matheus_o_viana@hotmail.com>
2022-12-05 22:57:05 -0300

    Day 10 of 100Days of SwiftUI, First lesson: learnt how to create structs and use em, also learnt how to mutate a property value of a struct

---
+++ Day10/Day10.playground/Contents.swift
@@ -0,0 +1,35 @@
+import Cocoa
+
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
+    
+    func printSummary() {
+        print("\(title) (\(year)) by \(artist)")
+    }
+}
+
+let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
+
+print(red)
+red.printSummary()
+
+struct Employee {
+    let name: String
+    var vacationRemaining: Int
+    
+    mutating func takeVacation(days: Int) {
+        if vacationRemaining > days {
+            vacationRemaining -= days
+            print("I'm going on vacation")
+            print("Days remaining: \(vacationRemaining)")
+        } else {
+            print("Oops! Tere aren't enough days remaining")
+        }
+    }
+}
+
+var archor = Employee(name: "Sterling Archer", vacationRemaining: 16)
+archor.takeVacation(days: 6)
+print(archor.vacationRemaining)
