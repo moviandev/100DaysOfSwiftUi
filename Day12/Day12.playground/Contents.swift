03a47192ed61b4af3c4fb62dbf2ac1c37aa1f054
Matheus Viana <matheus_o_viana@hotmail.com>
2022-12-17 14:02:34 -0300

    Day 12 of 100OfSwiftUI fourth lesson: learnt how to copy classes and do a safe copy

--- Day12/Day12.playground/Contents.swift
+++ Day12/Day12.playground/Contents.swift
@@ -67,3 +67,20 @@ class Car: Vehicle {
 }
 
 let teslaX = Car(isEletric: true, isConvertible: false)
+
+class User {
+    var username = "Anonymous"
+    
+    func copy() -> User {
+        let user = User()
+        user.username = username
+        return user
+    }
+}
+
+var user1 = User()
+var user2 = user1
+user2.username = "Taylor"
+
+print(user1.username)
+print(user2.username)
