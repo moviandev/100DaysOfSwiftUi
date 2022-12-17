c068c96fa3ebe709338570819693998b71d4d6c5
Matheus Viana <matheus_o_viana@hotmail.com>
2022-12-17 13:44:00 -0300

    Day 12 of 100OfSwiftUI second lesson: leanrt about how to inherit from another class how to make it final and no other classes can inherit from it, and learnt how to override a method in swift

--- Day12/Day12.playground/Contents.swift
+++ Day12/Day12.playground/Contents.swift
@@ -9,4 +9,42 @@ class Game {
 }
 
 var newGame = Game()
-newGame.score += 10;
+newGame.score += 10
+
+class Employee {
+    let hours: Int
+    
+    init(hours: Int) {
+        self.hours = hours
+    }
+    
+    func printSummary() {
+        print("I work \(hours) for day")
+    }
+}
+
+var newEmployee =  Employee(hours: 3)
+
+final class Developer: Employee {
+    func work() {
+        print("I'm writing code for \(hours) hours")
+    }
+}
+
+final class Manager: Employee {
+    func work() {
+        print("I'm going to meetings for \(hours) hours")
+    }
+    
+    override func printSummary() {
+        print("Test")
+    }
+}
+
+let robert = Developer(hours: 8)
+let jason =  Manager(hours: 10)
+
+robert.work()
+jason.work()
+robert.printSummary()
+jason.printSummary()
