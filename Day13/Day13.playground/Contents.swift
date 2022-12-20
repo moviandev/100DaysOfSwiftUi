13538e9015780143c91c4f06e8cf3f76243599af
Matheus Viana <matheus_o_viana@hotmail.com>
2022-12-20 19:19:14 -0300

    Day 13 of 100DaysOfSwiftUI first lesson: learnt about how to create and use extensions

--- Day13/Day13.playground/Contents.swift
+++ Day13/Day13.playground/Contents.swift
@@ -61,3 +61,50 @@ print(getRandomNumber() == getRandomNumber())
 
 let num = getRandomNumber()
 
+
+var quote = " yadda yada ydda "
+let trimmed = quote.trimmingCharacters(in: .whitespacesAndNewlines)
+
+extension String {
+    func trimmed() -> String {
+        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    mutating func trim() {
+        self = self.trimmed()
+    }
+    
+    var lines: [String] {
+        self.components(separatedBy: .newlines)
+    }
+}
+
+
+let trimeed1 = quote.trimmed()
+quote.trim()
+
+let lyrics = """
+But I keep cruising
+Can't stop, won't stop moving
+It's like I got this music in my mind
+Saying it's gonna be alright
+"""
+
+print(lyrics.lines)
+
+
+struct Book {
+    let title: String
+    let pageCount: Int
+    let readingHours: Int
+}
+
+extension Book {
+    init(title: String, pageCount: Int) {
+        self.title = title
+        self.pageCount = pageCount
+        self.readingHours = pageCount / 50
+    }
+}
+
+let lotr = Book(title: "Lord of the Rings", pageCount: 1178)
